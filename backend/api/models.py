cbddc275b0104dea83d5942490cad2c68230c901
Benji <someguynamedben7@gmail.com>
2019-11-15 23:29:44 -0700

    added the activity model and what not

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Activity(models.Model):
+    name = models.CharField(max_length=30)
+    discription = models.CharField(max_length=1000)
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField()
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    min_ages = models.IntegerField()
+    max_ages = models.IntegerField()
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+    attendies = models.IntegerField()
