90c751fbb592c821aa9e9ca01f5cd1b76d9b105c
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2019-11-15 22:50:15 -0700

    added profile model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField()
+    bio = models.TextField(max_length=200)
+    rating = models.DecimalField(decimal_places=2, max_digits=3)
+    picture = models.ImageField(upload_to='profileImages/')
